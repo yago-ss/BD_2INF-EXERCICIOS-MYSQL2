52ced7920358309b94bd4e62abe132c2a5f470ca
yago-ss <yago.cardoso2@aluno.ce.gov.br>
2025-10-31 21:21:41 -0300

    Add files via upload

---
+++ DDL e DML.sql
@@ -0,0 +1,91 @@
+Create DataBase Papelaria;
+Use Papelaria;
+
+Create Table Duplicata(
+
+Nome varchar(40),
+Numero int primary key, not null,
+Valor decimal (10,2),
+Vencimento Date,
+Banco varchar (10),
+);
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('ABC Papelaria, 100100, 5000, 20/01/2017, Itau')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Livraria Fernandes, 100110, 2500, 22/01/2017, Itau')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Livraria Fernandes, 100120, 1500, 15/10/2016, Bradesco')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('ABC Papelaria, 100130, 8000, 15/10/2016, Santander')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Ler e Saber, 200120, 10500, 26/04/2018, Banco do Brasil')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Livros e Cia, 200125, 2000, 26/04/2018, Banco do Brasil')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Ler e Saber, 200130, 11000, 26/09/2018, Itau')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Papelaria Silva, 250350, 1500, 26/01/2018, Bradesco')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Livros MM, 250360, 500, 18/12/2018, Santander')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Livros MM, 250370, 3400, 26/04/2018, Santander')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Papelaria Silva, 250380, 3500, 26/04/2018, Banco do Brasil')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Livros e Cia, 453360, 1500, 15/06/2018, Itau')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Livros MM, 453365, 5400, 15/06/2018, Bradesco')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Papelaria Silva, 453370, 2350, 27/12/2017, Itau')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Livros e Cia, 453380, 1550, 27/12/2017, Banco Do Brasil')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('ABC Papelaria, 980130, 4000, 11/12/2016, Itau')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Livraria Fernandes, 770710, 2500, 15/11/2016, Santander')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('ABC Papelaria, 985001, 3000, 11/09/2016, Itau')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Papel e Afins, 985002, 2500, 12/03/20166, Santander')
+
+insert into Papelaria
+(Nome, Numero, Valor, Vencimento, Banco)
+Values ('Ler e Saber, 888132, 2500, 05/03/2017, Itau')
